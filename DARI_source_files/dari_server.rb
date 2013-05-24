78b6f4367d513596f335824981048acc2d41672d
nnennaude <nnenna.udegbunam@gmail.com>
2013-05-24 15:50:49 -0600

    Update dari_server.rb
    removed python script
    added code for processing directly in ruby

--- DARI_source_files/dari_server.rb
+++ DARI_source_files/dari_server.rb
@@ -5,6 +5,18 @@ include Magick
 
 Tilt.register Tilt::ERBTemplate, 'html'
 
+class Array
+  def every(n)
+    select {|x| index(x) % n == 0}
+  end
+  def every_other
+    every 2
+  end
+  def pct(n)
+  	self[key * self.length/100]
+  end
+end
+
 get'/' do
 	"<h1>Welcome to the DARI Server!</h1><a href='/choose_data.htm'>data chooser UI</a><br><a href='/choose_data.htm'>data chooser UI</a>"
 end
@@ -33,12 +45,27 @@ post '/plot_selected' do
 end
 
 get '/plotter' do
-	result = ""
+	#result = ""
 	@temperature_a = ['1','2','3','4', '5'] #remove
-	@temperature_a.each do |inv|
-	    result << (inv + ",")
-	end
-	File.open('public/data.csv', 'w') { |file| file.write(result.chop) }
+	#@temperature_a.each do |inv|
+	#    result << (inv + ",")
+	#end
+	#File.open('public/data.csv', 'w') { |file| file.write(result.chop) }
+	@temperature_a.sort
+	array_info = [{
+		name: "unnamed series",
+		p5: arr.pct(5),
+		p95: arr.pct(95),
+		mean: @temperature_a.reduce(:+).to_f / @temperature_a.size,
+		median: arr.pct(50)
+		}]
+	result = {
+		arrays: [@temperature_a], 
+		series_names: ['unnamed series'], 
+		max: @temperature_a.last, 
+		min: @temperature_a.first,
+		array_info: array_info}
+
 
 	arr = IO.popen(["C:\\Python33\\python.exe", "public/process_data.py"]).gets
 	erb :summary_plots, :locals => {:arr => arr, :analysis => params[:Analysis], :os => params[:OS], :classification => params[:Classification]}
